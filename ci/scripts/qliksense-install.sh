6dc0bc2ac486b638f92a28f633ba13ed682edb40
mokassem <mohamed.kassem@usask.ca>
2020-01-23 14:40:50 -0500

    rotateKeys: yes

--- ci/scripts/qliksense-install.sh
+++ ci/scripts/qliksense-install.sh
@@ -18,7 +18,7 @@ setup_kubectl_context
 
 
 cat <<EOF > cr.tmpl.yaml
-configProfile: manifests/docker-desktop
+configProfile: manifests/base
 manifestsRoot: "/root/src"
 storageClassName: efs
 namespace: "$GENERATED_NAMESPACE"
